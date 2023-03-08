20319941c95c0e7dbea4324489356a591357e288
Raspberry Pi 4 <batterchox@gmail.com>
2023-03-07 20:21:01 -0500

    making it work

--- fireworkDisplay.py
+++ fireworkDisplay.py
@@ -1,45 +1,56 @@
+from re import X
 import time
 import os
 import sys
 import playsound
 
-
-sys.stdout.write("\x1b[8;{rows};{cols}t".format(rows=48, cols=80))
-
-
-class bcolors:
-    ENDC = '\033[0m'
-    GREEN = '\033[0;32m'
-    GOLD = '\033[1;33m'
-
-
-text_files = []
-files_exist = True
-folder_name = 'fireworks'
-num_found = 0
-startTime = round(time.time())
-endTime = startTime + 5
-goldRushASCII = ["░█▀▀░█▀█░█░░░█▀▄░", "░█▀▄░█░█░█▀▀░█░█░", "░█▀▄░█▀█░█▀▄░█▀█░▀█▀░▀█▀░█▀▀░█▀▀",
-                 "░█░█░█░█░█░░░█░█░", "░█▀▄░█░█░▀▀█░█▀█░", "░█▀▄░█░█░█▀▄░█░█░░█░░░█░░█░░░▀▀█",
-                 "░▀▀▀░▀▀▀░▀▀▀░▀▀░░", "░▀░▀░▀▀▀░▀▀▀░▀░▀░", "░▀░▀░▀▀▀░▀▀░░▀▀▀░░▀░░▀▀▀░▀▀▀░▀▀▀"]
-
-while files_exist:
-    file_name = folder_name + "/" + str(num_found) + ".txt"
-
-    if os.path.isfile(file_name):
-        f = open(file_name, "r")
-        text_files.append(f.read())
-        num_found += 1
+with open("check.txt", "r+") as f:
+    if(not f.readline()):
+        f.write("RUN")
+        os.system("lxterminal --geometry=80x48 --command='python3 fireworkDisplay.py'")
     else:
-        files_exist = False
+        class bcolors:
+            ENDC = '\033[0m'
+            GREEN = '\033[0;32m'
+            GOLD = '\033[1;33m'
 
-while round(time.time()) < endTime:
-    playsound.playsound("fireworkDisplay.mp3", False)
-    for text_file in text_files:
-        for i in [0, 3, 6]:
-            print(
-                f"{bcolors.GREEN}{goldRushASCII[i]}{bcolors.ENDC} {bcolors.GOLD}{goldRushASCII[i + 1]}{bcolors.ENDC} {bcolors.GREEN}{goldRushASCII[i + 2]}{bcolors.ENDC}")
 
-        print(text_file)
+        text_files = []
+        files_exist = True
+        folder_name = 'fireworks'
+        num_found = 0
+        startTime = round(time.time())
+        endTime = startTime + 5
+        goldRushASCII = ["░█▀▀░█▀█░█░░░█▀▄░", "░█▀▄░█░█░█▀▀░█░█░", "░█▀▄░█▀█░█▀▄░█▀█░▀█▀░▀█▀░█▀▀░█▀▀",
+                         "░█░█░█░█░█░░░█░█░", "░█▀▄░█░█░▀▀█░█▀█░", "░█▀▄░█░█░█▀▄░█░█░░█░░░█░░█░░░▀▀█",
+                         "░▀▀▀░▀▀▀░▀▀▀░▀▀░░", "░▀░▀░▀▀▀░▀▀▀░▀░▀░", "░▀░▀░▀▀▀░▀▀░░▀▀▀░░▀░░▀▀▀░▀▀▀░▀▀▀"]
 
-        time.sleep(.05)
+        while files_exist:
+            file_name = folder_name + "/" + str(num_found) + ".txt"
+
+            if os.path.isfile(file_name):
+                f = open(file_name, "r")
+                page = ""
+                
+                text_files.append(''.join(f.readlines()[5:]))
+                num_found += 1
+            else:
+                files_exist = False
+
+        while round(time.time()) < endTime:
+            playsound.playsound("fireworkDisplay.mp3", False)
+            for text_file in text_files:
+                os.system("clear")
+                print(text_file)
+                for i in [0, 3, 6]:
+                    print(
+                        f"{bcolors.GREEN}{goldRushASCII[i]}{bcolors.ENDC} {bcolors.GOLD}{goldRushASCII[i + 1]}{bcolors.ENDC} {bcolors.GREEN}{goldRushASCII[i + 2]}{bcolors.ENDC}")
+
+                
+
+                
+
+                time.sleep(.05)
+
+        open("check.txt", "w")
+
